326f7bb721561032aa53b644fce14981167fbba1
Olga Sokolinskaya <olga_genplan@hotmail.com>
2023-04-26 21:48:23 +0300

    Task1

--- src/main/java/com/andersen/QAAutoTraineeTask1.java
+++ src/main/java/com/andersen/QAAutoTraineeTask1.java
@@ -1,2 +1,34 @@
-package main.java.com.andersen;public class QAAutoTraineeTask1 {
+package main.java.com.andersen;
+
+import java.util.Scanner;
+
+public class QAAutoTraineeTask1 {
+
+    //Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+
+    public static double readFromConsole(Scanner scanner) {
+        try {
+            String s = scanner.nextLine();
+            return Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            System.out.println("это не число. Попробуйте еще раз");
+            return readFromConsole(scanner);
+        }
+    }
+
+    public static void sayHi() {
+        System.out.println("введите число");
+        Scanner scanner = new Scanner(System.in);
+        double number = QAAutoTraineeTask1.readFromConsole(scanner);
+        if (number > 7) {
+            System.out.println("Привет");
+        } else {
+            System.out.println("Попробуйте еще раз");
+            sayHi();
+        }
+    }
+
+    public static void main(String[] args) {
+        QAAutoTraineeTask1.sayHi();
+    }
 }
